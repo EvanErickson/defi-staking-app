43668097ca3bec1289c34f0e6323455c7ac13a35
Evan Erickson <evanserickson@gmail.com>
2022-03-08 16:47:16 -0600

    changes

--- defi-staking-app/migrations/2_deploy_contracts.js
+++ defi-staking-app/migrations/2_deploy_contracts.js
@@ -2,8 +2,19 @@ const Tether = artifacts.require('Tether')
 const RWD = artifacts.require('RWD')
 const DecentralBank = artifacts.require('DecentralBank')
 
-module.exports = async function(deployer){
+module.exports = async function(deployer, network, accounts){
     await deployer.deploy(Tether)
+    const tether = await Tether.deployed()
+
     await deployer.deploy(RWD)
+    const rwd = await RWD.deployed()
+
     await deployer.deploy(DecentralBank)
+    const decentralBank = await DecentralBank.deployed()
+
+    //Transfer RWD tokens to our bank
+    await rwd.transfer(DecentralBank.address, '1000000000000000000000000')
+
+    //Distribute 100 Tether tokens to investor
+    await tether.transfer(accounts[1], '1000000000000000000')
 }
