d0f43cf3757117cd589f0a8e37766bc66f00d524
Evan Erickson <evanserickson@gmail.com>
2022-03-13 12:17:51 -0500

    fixed issues with total supply and can now fetch balances and create / send with ganache

--- defi-staking-app/migrations/2_deploy_contracts.js
+++ defi-staking-app/migrations/2_deploy_contracts.js
@@ -9,12 +9,12 @@ module.exports = async function(deployer, network, accounts){
     await deployer.deploy(RWD)
     const rwd = await RWD.deployed()
 
-    await deployer.deploy(DecentralBank)
+    await deployer.deploy(DecentralBank, rwd.address, tether.address)
     const decentralBank = await DecentralBank.deployed()
 
     //Transfer RWD tokens to our bank
-    await rwd.transfer(DecentralBank.address, '1000000000000000000000000')
+    await rwd.transfer(decentralBank.address, '1000000000000000000000000')
 
     //Distribute 100 Tether tokens to investor
-    await tether.transfer(accounts[1], '1000000000000000000')
+    await tether.transfer(accounts[1], '1000000000000000000000000')
 }
